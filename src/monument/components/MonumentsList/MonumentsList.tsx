7bbaf2e3a7ca8bb5a3c07b8bb9ab15bf14687bbd
Raimon <raimon.txollo@gmail.com>
2025-04-10 22:58:39 +0200

    Add MonumentList function component and styles

---
+++ src/monument/components/MonumentsList/MonumentsList.tsx
@@ -0,0 +1,23 @@
+import Monument from "../../types";
+import MonumentCard from "../MonumentCard/MonumentCard";
+import "./MonumentsList.css";
+
+interface MonumentsListProps {
+  monuments: Monument[];
+}
+
+const MonumentsList: React.FC<MonumentsListProps> = ({ monuments }) => {
+  return (
+    <div className="monuments-container">
+      <ul className="monuments">
+        {monuments.map((monument) => (
+          <li className="monument-wrapper">
+            <MonumentCard monument={monument} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MonumentsList;
