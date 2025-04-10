10b73c79da7fff3e12f6ac1bdb42a9d5538ed612
Raimon <raimon.txollo@gmail.com>
2025-04-10 22:55:51 +0200

    Fix getMonument method from MonumentClient

--- src/monument/client/MonumentsClient.ts
+++ src/monument/client/MonumentsClient.ts
@@ -1,11 +1,12 @@
 import mapMonumentsDtoToMonuments from "../dto/mappers";
 import MonumentDto from "../dto/type";
+import Monument from "../types";
 import MonumentClientStructure from "./types";
 
 class MonumentClient implements MonumentClientStructure {
   private apiUrl = import.meta.env.VITE_API_URL;
 
-  public getMonuments = async (): Promise<MonumentDto[]> => {
+  public getMonuments = async (): Promise<Monument[]> => {
     const response = await fetch(`${this.apiUrl}/monuments`);
 
     const { monuments: monumentsDto } = (await response.json()) as {
