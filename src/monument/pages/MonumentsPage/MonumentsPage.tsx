20035ca963feaae1a9935c0d992fa976b7d077b6
Raimon <raimon.txollo@gmail.com>
2025-04-10 23:39:04 +0200

    Add useState and useEffect hook to MonumentsPage function component

--- src/monument/pages/MonumentsPage/MonumentsPage.tsx
+++ src/monument/pages/MonumentsPage/MonumentsPage.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+import Monument from "../../types";
+import MonumentClient from "../../client/MonumentsClient";
+import MonumentsList from "../../components/MonumentsList/MonumentsList";
+
 const MonumentsPage: React.FC = () => {
-  return <></>;
+  const [monuments, setMonuments] = useState<Monument[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const monumentClient = new MonumentClient();
+      const apiMonuments = await monumentClient.getMonuments();
+
+      setMonuments(apiMonuments);
+    })();
+  }, []);
+
+  return <MonumentsList monuments={monuments} />;
 };
 
 export default MonumentsPage;
