cf6f07679f19a850cc06f780f56ab4da66f0ebee
Raimon <raimon.txollo@gmail.com>
2025-04-10 23:37:44 +0200

    Add MonumentList function component test

--- src/monument/fixture.tsx
+++ src/monument/fixture.tsx
@@ -1,14 +1,32 @@
 import Monument from "./types";
 
-const santaMaria: Monument = {
+export const santaMaria: Monument = {
   id: "fff666",
   name: "Basílica de Santa María",
-  description: "Maravillosa la iglesia hoygan",
+  description: "Fantàstica l'església escolti",
   imageUrl: "",
   imageDescription:
-    "Vista de la façana principal de la Basílica de Santa María de Vilafranca del Penedès",
+    "Façana principal de la Basílica de Santa María de Vilafranca del Penedès",
   country: "Is Pain",
   city: "Vilafranca del Penedès",
 };
 
-export default santaMaria;
+export const ermitaCid: Monument = {
+  id: "ccc333",
+  name: "Ermita de la Virgen del Cid",
+  description: "La més millor ermita del Sistema Solar",
+  imageUrl: "",
+  imageDescription: "Vista general del pati de l'Ermita de la Virgen del Cid",
+  country: "Teruel",
+  city: "La Iglesuela del Cid",
+};
+
+export const bruc129: Monument = {
+  id: "bbb222",
+  name: "The Refactor Project Headquarters",
+  description: "L'oficina de 'Los Chavales'",
+  imageUrl: "",
+  imageDescription: "Detall de la cuina del pis, amb Lekué en primer pla",
+  country: "Code Land",
+  city: "Refactor City",
+};
