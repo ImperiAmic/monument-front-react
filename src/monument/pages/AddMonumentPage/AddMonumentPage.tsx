07ed878fe92daa3ca149d2ca15be8a96d60d45aa
Raimon <raimon.txollo@gmail.com>
2025-04-13 16:54:56 +0200

    Fix AddMonumentPage component to include MonumentForm

--- src/monument/pages/AddMonumentPage/AddMonumentPage.tsx
+++ src/monument/pages/AddMonumentPage/AddMonumentPage.tsx
@@ -1,9 +1,11 @@
+import MonumentForm from "../../components/MonumentForm/MonumentForm";
 import "./AddMonumentPage.css";
 
 const AddMonumentPage: React.FC = () => {
   return (
     <div className="form-container">
       <h2 className="form__title">Add a new monument</h2>
+      <MonumentForm />
     </div>
   );
 };
