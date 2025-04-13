de6f8437b016ca31d7d548b4b2c82e9ea607db80
Raimon <raimon.txollo@gmail.com>
2025-04-13 16:53:42 +0200

    Add MonumentsForm component and styles

---
+++ src/monument/components/MonumentForm/MonumentForm.tsx
@@ -0,0 +1,43 @@
+import "./MonumentForm.css";
+
+const MonumentForm: React.FC = () => {
+  return (
+    <form className="form">
+      <div className="form__group">
+        <label className="form__label" htmlFor="name">
+          Monument Name
+        </label>
+        <input className="form__input" type="text" id="name" required />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="description">
+          Monument Description
+        </label>
+        <input className="form__input" type="text" id="description" required />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="imageUrl">
+          Monument Image URL
+        </label>
+        <input className="form__input" type="url" id="imageUrl" required />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="city">
+          Monument City
+        </label>
+        <input className="form__input" type="text" id="city" required />
+      </div>
+      <div className="form__group">
+        <label className="form__label" htmlFor="country">
+          Monument Country
+        </label>
+        <input className="form__input" type="text" id="country" required />
+      </div>
+      <button className="form__button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default MonumentForm;
